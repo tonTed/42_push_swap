0cf34af49135ca12b8792cc644f581375ba043cf
Teddy Blanco <tblanco@student.42quebec.com>
2021-12-23 20:28:47 -0500

    :fire:[clean] reset functions

--- include/ft_push_swap.h
+++ include/ft_push_swap.h
@@ -3,26 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_push_swap.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 11:25:04 by tblanco           #+#    #+#             */
-/*   Updated: 2021/12/20 11:30:13 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/12/23 20:26:20 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PUSH_SWAP_H
 # define FT_PUSH_SWAP_H
+
 # include "stdbool.h"
 # include "../libft/include/libft.h"
-
-
-
-
-
-
-
-
-
 
 
 # include <stdio.h>
--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -3,71 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_push_swap.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 10:59:40 by tblanco           #+#    #+#             */
-/*   Updated: 2021/12/20 14:35:26 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/12/23 20:18:13 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
-static void	ft_error(void)
+int main(int argc, char **argv)
 {
-	ft_putendl_fd("Error", 1);
-	exit(1);
-}
-
-static void	check_number(char *nb, char **tab)
-{
-	if(*nb == '-' || *nb == "+" || ft_isdigit(*nb))
-		nb++;
-	while(*nb)
-	{
-		if(!(ft_isdigit(*nb)))
-		{
-			free(tab);
-			ft_error();
-		}
-		nb++;
-	}
-}
-
-static void check_input(int argc, char **argv)
-{
+	printf("Hello %s\n", argv[0]);
 	char	**args;
 	int		i;
 	
 	if(argc < 2)
-		exit(1);
+		return(EXIT_SUCCESS);
 	else if (argc == 2)
-		args = ft_split(argv[1], ' ');
+	{
+		args = ft_split(argv[1], SPACE);
+		ft_puttabstr_fd(args, 1);
+	}
 	else
 	{
-		args = (char **)malloc(sizeof(char*) * (argc));
-		if(!args)
-			exit(1);
+		args = (char**)malloc(sizeof(char*) * argc);
 		i = 0;
-		while (argc-- > 1)
+		while (i < (argc - 1))
 		{
-			args[i] = argv[i + 1];
+			printf("%s\n", argv[i + 1]);
 			i++;
 		}
 	}
-	ft_puttabstr_fd(args, 1);
-}
-
-int main(int argc, char **argv)
-{
-	
-	printf("Hello %s\n", argv[0]);
-	check_input(argc, argv);
-	
-	int i = 0;
-	while (argc-- > 1)
-	{
-		printf("%p\n", argv[i + 1]);
-		i++;
-	}
-	return 0;
+	return(EXIT_SUCCESS);
 }
