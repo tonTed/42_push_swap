67de04cfb5fcc95a8f9c89a5efb2ca87211ee998
Teddy Blanco <tblanco@student.42quebec.com>
2021-12-24 14:47:04 -0500

    :sparkles: function for create tabs of str with the input

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -6,34 +6,44 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 10:59:40 by tblanco           #+#    #+#             */
-/*   Updated: 2021/12/23 20:18:13 by tonted           ###   ########.fr       */
+/*   Updated: 2021/12/24 14:42:28 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
-int main(int argc, char **argv)
+char	**create_tabstr(char **argv, int argc)
 {
-	printf("Hello %s\n", argv[0]);
-	char	**args;
+	char	**tab;
 	int		i;
 	
 	if(argc < 2)
-		return(EXIT_SUCCESS);
+		return(NULL);
 	else if (argc == 2)
-	{
-		args = ft_split(argv[1], SPACE);
-		ft_puttabstr_fd(args, 1);
-	}
+		tab = ft_split(argv[1], SPACE);
 	else
 	{
-		args = (char**)malloc(sizeof(char*) * argc);
+		tab = (char**)malloc(sizeof(char*) * argc);
 		i = 0;
 		while (i < (argc - 1))
 		{
-			printf("%s\n", argv[i + 1]);
+			tab[i] = ft_strdup(argv[i + 1]);
 			i++;
 		}
+		tab[i] = NULL;
 	}
+	return (tab);
+}
+
+int main(int argc, char **argv)
+{
+	printf("Hello %s\n", argv[0]);
+	
+	char	**tab;
+	tab = create_tabstr(argv, argc);
+	if (!tab)
+		return(EXIT_FAILURE);
+
+	ft_freetabstr(tab);
 	return(EXIT_SUCCESS);
 }
