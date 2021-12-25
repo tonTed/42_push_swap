a8aaf718a79a04b2e88050430ed601e394e7773b
Teddy Blanco <tblanco@student.42quebec.com>
2021-12-24 21:40:47 -0500

    :construction: managing errors are good

--- include/push_swap.h
+++ include/push_swap.h
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_push_swap.h                                     :+:      :+:    :+:   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 11:25:04 by tblanco           #+#    #+#             */
-/*   Updated: 2021/12/23 20:26:20 by tonted           ###   ########.fr       */
+/*   Updated: 2021/12/24 21:18:05 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PUSH_SWAP_H
-# define FT_PUSH_SWAP_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-# include "stdbool.h"
 # include "../libft/include/libft.h"
 
+bool	isnumberzero(char *str);
+bool	isduplicatenumbers(int *tab, int len);
 
 # include <stdio.h>
 #endif
--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -6,24 +6,34 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 10:59:40 by tblanco           #+#    #+#             */
-/*   Updated: 2021/12/24 14:42:28 by tonted           ###   ########.fr       */
+/*   Updated: 2021/12/24 21:16:46 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_push_swap.h"
+#include "push_swap.h"
+
+void	freeexit(char ***tabstr, int **tabint)
+{
+	if (tabstr)
+		ft_freetabstr(tabstr);
+	if (tabint)
+		ft_freetabint(tabint);
+	ft_errorendl_fd(STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
 
 char	**create_tabstr(char **argv, int argc)
 {
 	char	**tab;
 	int		i;
-	
-	if(argc < 2)
-		return(NULL);
+
+	if (argc < 2)
+		return (NULL);
 	else if (argc == 2)
 		tab = ft_split(argv[1], SPACE);
 	else
 	{
-		tab = (char**)malloc(sizeof(char*) * argc);
+		tab = (char **)malloc(sizeof(char *) * argc);
 		i = 0;
 		while (i < (argc - 1))
 		{
@@ -32,18 +42,42 @@ char	**create_tabstr(char **argv, int argc)
 		}
 		tab[i] = NULL;
 	}
+	if (!ft_isnumberstab(tab))
+		freeexit(&tab, NULL);
 	return (tab);
 }
 
-int main(int argc, char **argv)
+int	*create_tabint(char **tabstr)
 {
-	printf("Hello %s\n", argv[0]);
-	
+	int		*tabint;
+	int		lentab;
+	int		i;
+
+	lentab = ft_strtablen(tabstr);
+	tabint = (int *)malloc(sizeof(int) * lentab);
+	i = 0;
+	while (i < lentab)
+	{
+		tabint[i] = ft_atoi(tabstr[i]);
+		if (tabint[i] == 0 && isnumberzero(tabstr[i]) == false)
+			freeexit(&tabstr, &tabint);
+		i++;
+	}
+	if (isduplicatenumbers(tabint, lentab))
+		freeexit(&tabstr, &tabint);
+	ft_freetabstr(&tabstr);
+	return (tabint);
+}
+
+int	main(int argc, char **argv)
+{
 	char	**tab;
+	int		*tabint;
+
 	tab = create_tabstr(argv, argc);
 	if (!tab)
-		return(EXIT_FAILURE);
-
-	ft_freetabstr(tab);
-	return(EXIT_SUCCESS);
+		return (EXIT_FAILURE);
+	tabint = create_tabint(tab);
+	free(tabint);
+	return (EXIT_SUCCESS);
 }
