66f46bdae21344a3db659aa7016cdd4e137bec7c
Teddy Blanco <tblanco@student.42quebec.com>
2021-12-25 22:21:18 -0500

    :sparkles: quicksort implemented

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 11:25:04 by tblanco           #+#    #+#             */
-/*   Updated: 2021/12/24 21:18:05 by tonted           ###   ########.fr       */
+/*   Updated: 2021/12/25 21:43:01 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 bool	isnumberzero(char *str);
 bool	isduplicatenumbers(int *tab, int len);
+void	quicksort(int **tab, int start, int end);
 
 # include <stdio.h>
 #endif
