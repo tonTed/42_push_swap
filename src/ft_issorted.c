fc479b32c32e9f7ab9ec7546242f5252cba32ab6
Teddy Blanco <tblanco@student.42quebec.com>
2021-12-25 22:33:19 -0500

    :sparkles: func for check if the tab is sorted

--- src/ft_issorted.c
+++ src/ft_issorted.c
@@ -1,24 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_issorted.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/19 11:25:04 by tblanco           #+#    #+#             */
-/*   Updated: 2021/12/25 22:32:40 by tonted           ###   ########.fr       */
+/*   Created: 2021/12/25 22:24:33 by tonted            #+#    #+#             */
+/*   Updated: 2021/12/25 22:32:13 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "libft.h"
 
-# include "../libft/include/libft.h"
+// #include <stdbool.h>
+// #include <stdio.h>
 
-bool	isnumberzero(char *str);
-bool	isduplicatenumbers(int *tab, int len);
-void	quicksort(int **tab, int start, int end);
-bool	ft_issorted(int *tab, size_t len);
+bool	ft_issorted(int *tab, size_t len)
+{
+	size_t	i;
 
-# include <stdio.h>
-#endif
+	i = 0;
+	while (i < len)
+	{
+		if (tab[i] > tab[i + 1] || tab[len - 1] < tab[len - 2])
+			return (true);
+		i++;
+		len--;
+	}
+	return (false);
+}
+
+/*
+int main()
+{
+	int tab[] = {1, 2, 5, 4};
+	printf("%d\n", ft_issorted(tab, 4));
+}
+*/
